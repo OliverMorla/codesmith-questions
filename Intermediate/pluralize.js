4a4964d940fbd5d24d5195ca2ad8d415fbe924df
OliverMorla <olivermiguel1129@gmail.com>
2023-11-28 15:40:55 -0500

    cleaning up code

---
+++ Intermediate/pluralize.js
@@ -0,0 +1,5 @@
+// ADD CODE HERE
+
+// Uncomment these to check your work!
+// const animals = ["dog", "cat", "tree frog"];
+// console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
